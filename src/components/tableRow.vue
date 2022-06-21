092a56fe33aba9d54b90ce08ae1aaed78b5e3912
Sebastian Klähn <scoreplayer2000@gmail.com>
2022-06-21 20:40:55 +0200

    yees

---
+++ src/components/tableRow.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+tr
+    td data.ts
+    td data.event_type
+    td data.data1
+    td data.data2
+
+</template>
+
+<script lang="ts" setup>
+import type { PropType } from 'vue';
+import type { usedData } from '~/composables/types';
+
+
+defineProps({
+    data: {} as PropType<usedData>
+})
+</script>
---
+++ src/composables/types.ts
@@ -0,0 +1,6 @@
+export interface usedData {
+    data1: any
+    data2: any
+    event_type: any
+    ts: Number
+}
