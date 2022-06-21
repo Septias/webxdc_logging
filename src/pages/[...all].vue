cffa2a5fad8ef92ee6da7c3f2fc4d06c55cc846c
Sebastian Klähn <scoreplayer2000@gmail.com>
2022-06-21 20:39:50 +0200

    smth

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,59 +1,19 @@
 <script setup lang="ts">
 import 'vue-good-table-next/dist/vue-good-table-next.css'
 
-import { VueGoodTable } from 'vue-good-table-next'
 import { ref } from 'vue'
 import type { ReceivedStatusUpdate } from 'webxdc'
 import { stump_data } from '~/stump'
 import { toggleDark } from '~/composables/dark'
-
-const columns = [
-  {
-    label: 'Timestamp',
-    field: 'ts',
-    type: 'date',
-    dateInputFormat: 'T',
-    dateOutputFormat: 'E H:m:ss',
-    sortable: false,
-  },
-  {
-    label: 'Type',
-    field: 'event_type',
-    type: 'number',
-    sortable: false,
-  },
-  {
-    label: 'Data 1',
-    field: 'data1',
-    type: 'number',
-    sortable: false,
-  },
-  {
-    label: 'Data 2',
-    field: 'data2',
-    type: 'number',
-    sortable: false,
-  },
-]
+import { VirtualList } from 'vue3-virtual-list';
+import tableRow from '~/components/tableRow.vue'
 
 const data = ref([] as usedData[])
-
-//https://borisflesch.github.io/vue-good-table-next/guide/configuration/pagination-options.html
-const pagination_options = {
-  enabled: true,
-  perPage: 20,
-  perPageDropdown: [20, 40, 60, 150],
-}
 
 // restore old logs from cache
 const cached_data = localStorage.getItem('log_data')
 if (cached_data) {
   data.value = JSON.parse(cached_data)
-}
-
-const options = {
-  enabled: true,
-  skipDiacritics: true,
 }
 
 interface LogData {
@@ -62,6 +22,7 @@ interface LogData {
   event_type: any
   ts: Number
 }
+
 
 interface usedData {
   data1: any
@@ -105,32 +66,21 @@ div
     h1.text-2xl.leading-none.mb-1 Current device logs
     button(@click="() => toggleDark()" key="2")
           div(i="carbon-sun dark:carbon-moon")
-  div
-    VueGoodTable(
-      :columns="columns"
-      :rows="data"
-      :search-options = "options"
-      :pagination-options = "pagination_options"
-      compactMode
-    )
+  
+  table
+    VirtualList( :data="data")
+      template(v-slot="{item, index}")
+        tr
+          td {{item.ts}}
+          td {{item.event_type}}
+          td {{item.data1}}
+          td {{item.data2}}
+
+    
 </template>
 
 <style lang="sass">
 
-table.vgt-table td
-  padding: 2px !important
-
-thead
-  display: none
-
-.vgt-right-align
-  text-align: left !important
-
-.vgt-global-search__input
-  padding-left: 5px !important
-
-.magnifying-glass
-  display: none !important
 
 .dark body 
   background: var(--dark-bg) !important
@@ -141,7 +91,6 @@ thead
 
 .dark table.vgt-table td 
   color: white !important
-
 
 .dark .vgt-global-search 
   background:none
